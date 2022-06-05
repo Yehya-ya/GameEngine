69c6a2195e686b4c646667f0d96a11bbea528d85
Yehya-ya <yahya.yaser@scantitan.com>
2022-06-06 01:12:13 +0300

    fix ShaderProgram

--- src/main/java/Engine/Renderer/ShaderProgram.java
+++ src/main/java/Engine/Renderer/ShaderProgram.java
@@ -1,19 +1,36 @@
 package Engine.Renderer;
 
+import Platforms.OpenGL.OpenGLShaderProgram;
+
+import static Engine.Utils.YH_Log.YH_ASSERT;
+import static Engine.Utils.YH_Log.YH_LOG_ERROR;
+
 public abstract class ShaderProgram {
     protected final String fragmentShaderFile;
     protected final String vertexShaderFile;
     protected int shaderProgramID;
 
-    public ShaderProgram(String vertexShaderFile, String fragmentShaderFile) {
+    protected ShaderProgram(String vertexShaderFile, String fragmentShaderFile) {
         this.vertexShaderFile = vertexShaderFile;
         this.fragmentShaderFile = fragmentShaderFile;
         compile();
     }
 
+    public static ShaderProgram create(String vertexShaderFile, String fragmentShaderFile) {
+        switch (RendererCommandAPI.getApi()) {
+            case OpenGL -> {
+                return new OpenGLShaderProgram(vertexShaderFile, fragmentShaderFile);
+            }
+        }
+
+        YH_LOG_ERROR("unknown renderer type {}", RendererCommandAPI.getApi().name());
+        YH_ASSERT(false, "unknown renderer type");
+        return null;
+    }
+
     public abstract void compile();
 
-    public abstract void destroy();
+    public abstract void delete();
 
     public abstract void bind();
 
