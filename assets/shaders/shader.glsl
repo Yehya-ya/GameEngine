425909e849b914469fa3efc86dc88d14bfb03479
Yehya-ya <yahya.yaser@scantitan.com>
2022-06-09 23:28:02 +0300

    adding transformation matrix to shaders

--- assets/shaders/shader.glsl
+++ assets/shaders/shader.glsl
@@ -2,9 +2,11 @@
 #version 330 core
 layout (location=0) in vec2 aPos;
 
+uniform mat4 uTransformation;
+
 void main()
 {
-    gl_Position = vec4(aPos,0.0f, 1.0);
+    gl_Position = uTransformation * vec4(aPos,0.0f, 1.0);
 }
 
 #type fragment
